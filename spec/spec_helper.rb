697675ed3bc714e2b973ead34a5d5818b3e9f036
Avi Tzurel <avi@kensodev.com>
2012-08-12 18:59:03 +0300

    Adding spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,64 @@
+AILS_ENV"] ||= 'test'
+
+require 'rubygems'
+require 'spork'
+require 'fabrication'
+require 'faker'
+require 'vcr'
+
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Fabrication.configure do |config|
+  config.fabricator_path = 'spec/fabricators'
+  config.path_prefix = "."
+end
+
+Spork.prefork do
+  # This file is copied to spec/ when you run 'rails generate rspec:install'  
+  require File.expand_path("../../spec/dummy/config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  VCR.configure do |c|
+    c.cassette_library_dir = 'fixtures/vcr_cassettes'
+    c.hook_into :fakeweb
+  end
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+
+    # ## Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
+    
+    config.before(:each) do 
+      Mongoid.purge!
+    end
+  end
+end
+
+Spork.each_run do
+
+end

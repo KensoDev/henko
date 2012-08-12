ae995924360a43ca828071e042d8394a7ea2d898
Avi Tzurel <avi@kensodev.com>
2012-08-12 19:12:56 +0300

    Adding the generators configuration

--- lib/change_logger/engine.rb
+++ lib/change_logger/engine.rb
@@ -1,5 +1,12 @@
 module ChangeLogger
   class Engine < ::Rails::Engine
     isolate_namespace ChangeLogger
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+      g.orm :mongoid
+      g.fixture_replacement :fabrication
+    end
   end
 end
